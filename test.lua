63dab12a1b1f43942cc06f6336e5e38123bfd75b
pewdssss <70344562+pewdssss@users.noreply.github.com>
2020-09-26 08:58:13 -0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,6 +1,6 @@
 local test_cb = ui.new_checkbox("lua", "b", "test checkbox")
 client.set_event_callback("console_input", function(txt)
-  if txt == "test_input" then
-    client.color_log(255, 255, 255, "Test")
+    if txt == "test_input" then
+        client.color_log(255, 255, 255, "Test")
     end
 end)
